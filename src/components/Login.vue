d2b66716a8eed0b7400ecc56944e3db6ddf2c391
youmargin <youmargin@126.com>
2020-03-14 17:33:25 +0800

    登录功能进一步完善

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -13,7 +13,8 @@
                 </el-form-item>
                 <!-- 密码 -->
                 <el-form-item prop="password">
-                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"
+                              type="password"></el-input>
                 </el-form-item>
                 <!-- 按钮区域 -->
                 <el-form-item class="btns">
@@ -38,13 +39,13 @@
                 loginFormRules: {
                     // 验证用户名是否合法
                     username: [
-                        { required: true, message: '请输入登录名称', trigger: 'blur' },
-                        { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+                        {required: true, message: '请输入登录名称', trigger: 'blur'},
+                        {min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur'},
                     ],
                     // 验证密码是否合法
                     password: [
-                        { required: true, message: '请输入登录密码', trigger: 'blur' },
-                        { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+                        {required: true, message: '请输入登录密码', trigger: 'blur'},
+                        {min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur'}
                     ]
                 }
             }
@@ -53,12 +54,12 @@
             // 点击重置按钮，重置登录表单
             resetLoginForm() {
                 // console.log(this);
-                this.$refs.loginFormRef.resetFields()
+                this.$refs.loginFormRef.resetFields();
             },
             login() {
                 this.$refs.loginFormRef.validate(async valid => {
                     if (!valid) return
-                    const { data: res } = await this.$http.post('login', this.loginForm)
+                    const {data: res} = await this.$http.post('login', this.loginForm)
                     if (res.meta.status !== 200) return this.$message.error('登录失败！')
                     this.$message.success('登录成功')
                     // 1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
@@ -66,7 +67,7 @@
                     //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
                     window.sessionStorage.setItem('token', res.data.token)
                     // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
-                    this.$router.push('/home')
+                    this.$router.push('/home');
                 })
             }
         }
@@ -100,6 +101,7 @@
             left: 50%;
             transform: translate(-50%, -50%);
             background-color: #fff;
+
             img {
                 width: 100%;
                 height: 100%;
--- src/main.js
+++ src/main.js
@@ -7,12 +7,13 @@ import ElementUI from 'element-ui';
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
+//element-ui 样式
 import 'element-ui/lib/theme-chalk/index.css';
 
-// import axios from 'axios'
+import axios from 'axios'
 // 配置请求的跟路径
-// axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-// Vue.prototype.$http = axios
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
