b116d90ec77e8309b0d5d3987546d07077ce26e1
youmargin <youmargin@126.com>
2020-03-14 18:20:08 +0800

    左侧导航菜单

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,40 @@
 
         </el-header>
         <el-container>
-            <el-aside>aside</el-aside>
+            <el-aside>
+                <el-menu
+                        class="el-menu-vertical-demo"
+                        background-color="#545c64"
+                        text-color="#fff"
+                        active-text-color="#ffd04b">
+                    <!--  一级菜单-->
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>导航一</span>
+                        </template>
+                        <!--  二级菜单-->
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-apple"></i>选项1
+                        </el-menu-item>
+                        <el-menu-item index="1-2">
+                            <i class="el-icon-bell"></i>选项2
+                        </el-menu-item>
+                    </el-submenu>
+                    <el-menu-item index="2">
+                        <i class="el-icon-menu"></i>
+                        <span slot="title">导航二</span>
+                    </el-menu-item>
+                    <el-menu-item index="3" disabled>
+                        <i class="el-icon-document"></i>
+                        <span slot="title">导航三</span>
+                    </el-menu-item>
+                    <el-menu-item index="4">
+                        <i class="el-icon-setting"></i>
+                        <span slot="title">导航四</span>
+                    </el-menu-item>
+                </el-menu>
+            </el-aside>
             <el-main>min</el-main>
         </el-container>
     </el-container>
