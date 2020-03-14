2c6f973995f4b967f036e543179340e6e8cf465e
youmargin <youmargin@126.com>
2020-03-14 18:26:38 +0800

    http请求增加token

--- src/main.js
+++ src/main.js
@@ -12,7 +12,14 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 import axios from 'axios'
 // 配置请求的跟路径
-axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/';
+//axios请求头增加token
+axios.interceptors.request.use(config => {
+    // console.log(config)
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    // 在最后必须 return config
+    return config;
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
@@ -20,6 +27,6 @@ Vue.config.productionTip = false
 Vue.use(ElementUI);
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
