699af62956a3333ad25fc05d69d18f03f1421514
youmargin <youmargin@126.com>
2020-03-17 09:21:53 +0800

    权限列表完成

--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,7 @@ import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from "../components/Welcome";
 import Users from "../components/ user/Users";
+import Rights from "../components/power/Rights";
 
 Vue.use(Router)
 
@@ -17,7 +18,8 @@ const router = new Router({
             redirect: '/welcome',
             children: [
                 {path: '/welcome', component: Welcome},
-                {path: '/users/', component: Users}
+                {path: '/users/', component: Users},
+                {path: '/rights', component: Rights}
             ]
         }
     ]
